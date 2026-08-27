7be8eb4043114d3972b4d7daa67594c7f46599a2
kevinmfwakley23-ux <kevinmfwakley23@gmail.com>
2026-08-27 03:39:02 -0600

    fix: correct manuscript production exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export type { MemoryClass, MemoryAuthority, MemoryProvenance, MemoryRecord, MemoryQuery } from "./domain/memory";
 export { createMemoryRecord, MEMORY_FORMAT_VERSION } from "./domain/memory";
 export { ProjectMemoryStore } from "./application/project-memory-store";
-export type { MemoryPromotionDecision, ProjectBrainContext, ProjectBrainQuery, ProjectMemorySnapshot } from "./application/project-memory-store";
+export type { MemoryPromotionDecision, ProjectMemorySnapshot } from "./application/project-memory-store";
 export { assembleProjectBrainContext } from "./application/project-brain";
 export type { ProjectBrainQuery, ProjectBrainContext } from "./application/project-brain";
 export { createProject, touchProject, withProjectMemories, withProjectCharacters, withProjectVisualIdentities, withProjectIllustrationAssetLibrary, withProjectBookCoverPlans, PROJECT_FORMAT_VERSION } from "./domain/project";
