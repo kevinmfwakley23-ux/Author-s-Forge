a577bbb858d62280258e8a26f3677ef792578de9
kevinmfwakley23-ux <kevinmfwakley23@gmail.com>
2026-08-27 00:33:40 -0600

    feat: expose complete Mission 004 Project Brain APIs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 export type { MemoryClass, MemoryAuthority, MemoryProvenance, MemoryRecord, MemoryQuery } from "./domain/memory";
 export { createMemoryRecord, MEMORY_FORMAT_VERSION } from "./domain/memory";
 export { ProjectMemoryStore } from "./application/project-memory-store";
-export type { MemoryPromotionDecision } from "./application/project-memory-store";
+export type { MemoryPromotionDecision, ProjectMemorySnapshot } from "./application/project-memory-store";
 export { assembleProjectBrainContext } from "./application/project-brain";
 export type { ProjectBrainQuery, ProjectBrainContext } from "./application/project-brain";
+export { createProject, touchProject, withProjectMemories, PROJECT_FORMAT_VERSION } from "./domain/project";
+export type { ProjectMetadata, ProjectState, ProjectStatus } from "./domain/project";
+export { FileProjectStore } from "./infrastructure/file-project-store";
