81dc2b0f835571d01af2b218aeca7f3c9891974d
kevinmfwakley23-ux <kevinmfwakley23@gmail.com>
2026-08-30 04:06:35 -0600

    test(api): enforce AI model broker public export

--- test/public-api-exports.test.js
+++ test/public-api-exports.test.js
@@ -8,7 +8,7 @@ const requiredFunctions = [
   "createBookSnapshot", "createSeries", "createVoiceProfile", "analyzeVoice",
   "createAiCollaborationPolicy", "createProjectHealthReport", "createMemoryRelationship",
   "createDeliveryAuditReport", "advanceProjectWorkflow", "AiWritingCoordinator",
-  "AiWritingStudioService", "AiEditingProposalService"
+  "AiWritingStudioService", "AiEditingProposalService", "AiModelBroker"
 ];
 
 const requiredConstructors = ["FileProjectStore", "AuthorControlService"];
