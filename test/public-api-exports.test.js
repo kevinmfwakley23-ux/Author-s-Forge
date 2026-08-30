b4dd4dd49b478ca3c2c6e9b8560d91094112a7d6
kevinmfwakley23-ux <kevinmfwakley23@gmail.com>
2026-08-30 00:45:12 -0600

    test(api): guard canonical Forge public exports

---
+++ test/public-api-exports.test.js
@@ -0,0 +1,18 @@
+const test = require("node:test");
+const assert = require("node:assert/strict");
+const forge = require("../.forge-build/index.js");
+
+const requiredFunctions = [
+  "createManuscriptState", "createBook", "createChapter", "createScene",
+  "createProject", "createProjectPackage", "createPublishingReadinessReport",
+  "createBookSnapshot", "createSeries", "createVoiceProfile", "analyzeVoice",
+  "createAiCollaborationPolicy", "createProjectHealthReport", "createMemoryRelationship",
+  "createDeliveryAuditReport", "advanceProjectWorkflow"
+];
+
+const requiredConstructors = ["FileProjectStore", "AuthorControlService"];
+
+test("public Forge API exports all canonical domain and application capabilities", () => {
+  for (const name of requiredFunctions) assert.equal(typeof forge[name], "function", `${name} must be exported as a function`);
+  for (const name of requiredConstructors) assert.equal(typeof forge[name], "function", `${name} must be exported as a constructor`);
+});
