7e006e2b6425f685a035c03c78b0dc0e0ed337ec
kevinmfwakley23-ux <kevinmfwakley23@gmail.com>
2026-08-29 21:46:01 -0600

    feat(context): expose integrated optimization service through application barrel

---
+++ src/application/context-optimization.ts
@@ -0,0 +1,6 @@
+export { ContextOptimizationService } from "./context-optimization-service";
+export type { OptimizeContextRequest, OptimizeContextResult } from "./context-optimization-service";
+export { createProductionContextEngineRegistry, CONTEXT_ENGINE_CAPABILITIES } from "./context-engine-stack";
+export type { ContextEngineCapability } from "./context-engine-stack";
+export { InMemoryContextOptimizationLedger } from "./context-optimization-ledger";
+export type { ContextOptimizationLedger, ContextOptimizationLedgerEntry } from "./context-optimization-ledger";
