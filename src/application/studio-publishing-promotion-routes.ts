3e6bb0f819ea88318147df706ddc75a7d6d00bd2
kevinmfwakley23-ux <kevinmfwakley23@gmail.com>
2026-09-01 17:50:05 -0600

    Wire Image Lab routes into current Studio server

--- src/application/studio-publishing-promotion-routes.ts
+++ src/application/studio-publishing-promotion-routes.ts
@@ -1,15 +1,18 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
 import { FileProjectStore } from "../infrastructure/file-project-store";
+import { createStudioImageLabRoutes } from "./studio-image-lab-routes";
 import { createStudioMarketPromotionRoutes } from "./studio-market-promotion-routes";
 import { createStudioPublishingRoutes } from "./studio-publishing-routes";
 
 export type StudioPublishingPromotionRouteHandler = (req: IncomingMessage, res: ServerResponse, url: URL, projectId: string) => Promise<boolean>;
 
 export function createStudioPublishingPromotionRoutes(store: FileProjectStore): StudioPublishingPromotionRouteHandler {
+  const imageLab = createStudioImageLabRoutes(store);
   const publishing = createStudioPublishingRoutes(store);
   const marketPromotion = createStudioMarketPromotionRoutes(store);
 
   return async (req, res, url, projectId) => {
+    if (await imageLab(req, res, url, projectId)) return true;
     if (await publishing(req, res, url, projectId)) return true;
     return marketPromotion(req, res, url, projectId);
   };
