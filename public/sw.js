6b4c859b5f6176e21b6e2307b5d51391356dc323
kevinmfwakley23-ux <kevinmfwakley23@gmail.com>
2026-09-02 13:54:16 -0600

    Cache manuscript intake extension in Forge PWA shell

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "authors-forge-shell-v11";
+const CACHE = "authors-forge-shell-v12";
 const SHELL = [
   "/",
   "/index.html",
@@ -13,6 +13,7 @@ const SHELL = [
   "/forge-chapter-cards.js",
   "/forge-chapter-card-workflow.js",
   "/forge-chapter-card-approval.js",
+  "/forge-manuscript-import.js",
   "/forge-series.js",
   "/forge-image-lab.js",
   "/forge-pwa.js",
