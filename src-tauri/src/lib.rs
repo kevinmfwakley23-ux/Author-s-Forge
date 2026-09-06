cab1c089976a3fbc48430591013cdd75bbec9bc9
kevinmfwakley23-ux <kevinmfwakley23@gmail.com>
2026-09-06 05:46:47 -0600

    feat: expose native Forge runtime status commands

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,12 @@
+mod runtime;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            runtime::native_runtime_status,
+            runtime::native_offices,
+        ])
         .run(tauri::generate_context!())
-        .expect("error while running Author's Forge native application");
+        .expect("error while running K.I.N.G.S. Author's Forge native application");
 }
